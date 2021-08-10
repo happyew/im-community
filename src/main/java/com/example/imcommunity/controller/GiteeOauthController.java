5bac4c39e8cfa5185c066d0d0dcb3e0be4d0b4c3
happyew <645339328@qq.com>
2021-08-10 22:20:47 +0800

    把gitee相关配置移到application.properties

--- src/main/java/com/example/imcommunity/controller/GiteeOauthController.java
+++ src/main/java/com/example/imcommunity/controller/GiteeOauthController.java
@@ -6,6 +6,7 @@ import com.example.imcommunity.dto.GiteeUserDTO;
 import com.example.imcommunity.dto.GithubUserDTO;
 import com.example.imcommunity.provider.GiteeProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GiteeOauthController {
     @Autowired
     GiteeProvider giteeProvider;
+    @Value("${gitee.client.id}")
+    private String clientId;
+    @Value("${gitee.client.secret}")
+    private String clientSecret;
+    @Value("${gitee.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/login")
-    public String login(@RequestParam(name = "code") String code){
+    public String login(@RequestParam(name = "code") String code) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClientSecret("2d2dcf2d77ca099d0a579873d70adeebd9043e992094b614ea0f50375d64f520");
-        accessTokenDTO.setClientId("eb00a0698b705d167196002dfe4392d69aad1b743525c72a6b617b5a8acece40");
-        accessTokenDTO.setRedirectUri("http://localhost:8080/login");
+        accessTokenDTO.setClientId(clientId);
+        accessTokenDTO.setClientSecret(clientSecret);
+        accessTokenDTO.setRedirectUri(redirectUri);
         String accessToken = giteeProvider.getAccessToken(accessTokenDTO);
         GiteeUserDTO user = giteeProvider.getUser(accessToken);
         System.out.println(user);
