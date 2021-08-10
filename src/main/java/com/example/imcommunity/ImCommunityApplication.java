a9221fed4a75d5f128b2439b31bc33b900dc3bdb
happyew <645339328@qq.com>
2021-08-10 15:06:41 +0800

    初始化项目，添加首页导航栏

---
+++ src/main/java/com/example/imcommunity/ImCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.imcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imcommunity/ImCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
